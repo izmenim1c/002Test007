9c4add27948801f5f5f51d4ff6490ae3abcd1a9e
izmenim1c <izmenim1c.ru@ya.ru>
2022-02-10 20:50:43 +0300

    создал первый фрагмент пустой и вывел его

---
+++ app/src/main/java/com/example/a002test007/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a002test007;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().
+                beginTransaction().
+                add(R.id.fragment_container,SettingsFragment.newInstance()).
+                commit();
+    }
+}
---
+++ app/src/main/java/com/example/a002test007/SettingsFragment.java
@@ -0,0 +1,34 @@
+package com.example.a002test007;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+    public static SettingsFragment newInstance()  {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+        return rootView;
+    }
+}
+
+
+
+
