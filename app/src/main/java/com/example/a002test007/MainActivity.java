8e52b1010f4c70be928b9d93135930aff9a6c55c
izmenim1c <izmenim1c.ru@ya.ru>
2022-02-16 22:18:11 +0300

    Добавил фрагмент Задачи, две кнопки и обработчики нажатий - открытие фрагментов через add

--- app/src/main/java/com/example/a002test007/MainActivity.java
+++ app/src/main/java/com/example/a002test007/MainActivity.java
@@ -1,18 +1,50 @@
 package com.example.a002test007;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().
-                beginTransaction().
-                add(R.id.fragment_container,SettingsFragment.newInstance()).
-                commit();
+        initView();
+    }
+
+    private void initView() {
+
+        Button button_settings = findViewById(R.id.button_settings);
+        Button button_task = findViewById(R.id.button_task);
+
+        button_settings.setOnClickListener(this);
+        button_task.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_task:
+                showFragment(TaskFragment.newInstance());
+                break;
+            case R.id.button_settings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .add(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/a002test007/TaskFragment.java
@@ -0,0 +1,33 @@
+package com.example.a002test007;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TaskFragment extends Fragment {
+
+    public static TaskFragment newInstance()  {
+        return new TaskFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_task, container, false);
+
+
+        return rootView;
+    }
+}
+
+
+
+
