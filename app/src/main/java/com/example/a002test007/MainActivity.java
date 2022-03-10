a98cca8f597aabf5f9f185cbf51e3bf3423ad3c1
izmenim1c <izmenim1c.ru@ya.ru>
2022-03-10 20:55:10 +0300

    Акшен бар удалил  из макета. Добавил програмно и вывел в него меню.

--- app/src/main/java/com/example/a002test007/MainActivity.java
+++ app/src/main/java/com/example/a002test007/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.a002test007;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +20,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initToolbar();
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_main:
+                //showFragment(TaskFragment.newInstance());
+                break;
+            case R.id.action_task:
+                showFragment(TaskFragment.newInstance());
+                break;
+            case R.id.action_settings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void initView() {
